c2738370249cafc969e4d9813727f749cb72ae97
Vitor Fernandes <131718485+vitorfer10@users.noreply.github.com>
2023-04-24 18:45:57 -0300

    Add files via upload

---
+++ Questão 5 Target Sistemas.py
@@ -0,0 +1,9 @@
+#Questao 5 Target Sistemas
+
+string = input("Digite uma string: ")
+string_invertida = ""
+
+for i in range(len(string)-1, -1, -1):
+    string_invertida += string[i]
+
+print("A string invertida é:", string_invertida)
